f8be4921ad49eec5c111034a754927b11e76a830
Steven Lu <tacticalazn@gmail.com>
2012-03-24 18:16:44 -0400

    adding framework

---
+++ application/views/include/header.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" 
+"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html lang="en">
+<head>
+   <meta http-equiv="Content-Type" content="text/html" charset="utf-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <meta name="description" content="Essay Beefer - All of the beef, none of the bull.">
+   <meta name="keywords" content="essay beefer improve cite">
+   <meta name="author" content="Steven Lu, Wayne Sun, Russell Frank, Jarek Sedlacek">
+
+   <title>Essay Beefer - All of the beef, none of the bull.</title>
+
+   <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+   <script src="<?= base_url('assets/js/bootstrap.min.js') ?>"></script>
+   <script src="<?= base_url('assets/js/custom.js') ?>"></script>
+
+   <link href="<?= base_url('assets/css/bootstrap.min.css') ?>" rel="stylesheet" />
+   <link href="<?= base_url('assets/css/custom.css') ?>" rel="stylesheet" />
+</head>
+
+<body>
