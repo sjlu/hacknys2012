4143bea577729d64921f45d7b8d6bc5d8e51df00
Jarek Sedlacek <jareksedlacek@gmail.com>
2012-03-24 18:59:48 -0400

    essay_models.php now actually pulls out entities (still needs to count them and such)

--- application/models/essay_model.php
+++ application/models/essay_model.php
@@ -1,18 +1,18 @@
 <?php
 
-class Essay_Model extends Model{
+class Essay_Model extends CI_Model{
 
     private $PARSELY_TIMEOUT_SECONDS = 15;
     private $PARSELY_API_ROOT = "http://hack.parsely.com";
 
     public function Essay_Model(){
-        parent::Model(); 
+        parent::__construct();
     }
 
     public function extract_entities($essay_text){
         //post the essay to parse.ly
-        $url = "$PARSELY_API_ROOT/parse";
-        $result = post(array('text'=>$essay_text, 'wiki_filter' => 'false'));
+        $url = $this->PARSELY_API_ROOT . "/parse";
+        $result = self::post($url,array('text'=>$essay_text, 'wiki_filter' => 'false'));
         // if post went through
         if ($result ){
             // decode the json, get the url of the result
@@ -21,31 +21,31 @@ class Essay_Model extends Model{
                 //check the results until they are ready (or we timeout) 
                 $seconds_waited = 0;      
                 $seconds_to_sleep = 1;
-                while ($time_waited <= $TIMEOUT){
-                   $job_result = file_get_contents($PARSELY_API_ROOT . $result['url']);
-                   $job_result = json_decode($entities,true);
+                while ($seconds_waited <= $this->PARSELY_TIMEOUT_SECONDS){
+                   $job_result = file_get_contents($this->PARSELY_API_ROOT . $result['url']);
+                   $job_result = json_decode($job_result,true);
                    //if not done yet, wait a second and ask again
                    if ($job_result['status'] === "WORKING"){
                         sleep($seconds_to_sleep);
                         $seconds_waited += $seconds_to_sleep;
                     }else{
                         // done working, it should return the extracted entities 
-                        $text_with_entities = json_decode($job_result,true);
-                        $text_with_entities = $text_with_entities['data'];
+                        $text_with_entities = $job_result['data'];
                         //now, we have to extract and count all the entities
-                        preg_match_all("/<TOPIC>(.*?)</TOPIC>/g",$text_with_entities,$entities,PREG_PATTERN_ORDER);
+                        preg_match_all("/<TOPIC>(.*?)<\/TOPIC>/",$text_with_entities,$entities,PREG_PATTERN_ORDER);
                         return $entities[1];
                         //foreach($entities as $entity){
                        // }
 
                     }
                 }
+            //result['url'] is false
             }else{
-                return false;
-                //error
+                return $result; 
             }
+        //post returned false
         }else{
-            return false;
+            return "post to $url failed!";
             //error!
         }
 
