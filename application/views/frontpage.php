0bea870c8e8c1f3957f39528cb712686d2d7d693
Steven Lu <tacticalazn@gmail.com>
2012-03-24 20:06:45 -0400

    Added some logo and content bg for the frontpage

--- application/views/frontpage.php
+++ application/views/frontpage.php
@@ -1,1 +1,23 @@
+<div id="header">
+   <div id="logo">
+      <img src="<?= base_url('assets/img/logo.png') ?>" alt="logo" /> 
+   </div>
 
+   <div class="infobox" id="infobox-1">
+      <h3>What we provide</h3>
+      This box contains a bunch of text which has a bunch of text and more text blah blah blah blah blah blah blah 
+   </div>
+
+   <div class="infobox" id="infobox-2">
+      <h3>A HackNY Spring 2012 Project</h3>
+      On <a href="https://github.com/sjlu/hacknys2012">Github</a>, developed by: 
+      <br />Steven Lu (<a href="https://twitter.com/stevenlu">@stevenlu</a>), 
+      <br />Russell Frank (<a href="https://twitter.com/russjf">@russjf</a>), 
+      <br />Wayne Sun (<a href="https://twitter.com/_waynesun">@_waynesun</a>),
+      <br />and Jarek Sedlacek.
+   </div>
+</div>
+
+<div id="input">
+
+</div>
