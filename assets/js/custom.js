94ddb150b20ce2fd39617fb60483e3fa1a0bd6a9
Steven Lu <tacticalazn@gmail.com>
2012-03-24 23:07:07 -0400

    making js look nicer and more organized

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,4 +1,56 @@
 /*
+ * Essay Beefer: custom.js
+ * @author: sjlu
+ */
+
+/*
+ * Contains all functions and actions relevant
+ * to the frontpage in a nice substanciated function
+ */
+function Frontpage () {
+   var exports = {};
+
+   function run_ajax (data) {
+      $.ajax({
+         url: 'index.php/api',
+         type: 'POST',
+         async: true,
+         dataType: 'json',
+         timeout: 25000,
+         data: {data: JSON.stringify(data)},
+         success: function(data)
+         {
+            // parsing response from api
+            alert(data.essay);
+         }
+      });
+   }
+
+   function continue_click () {
+      $('#page-index').fadeOut(function()
+      {
+         $('#page-loading').fadeIn(function()
+         {
+            // creating an object to send to api
+            var data_obj = {};
+            data_obj.essay = $('#essay-text').val();
+
+            // ajax request
+            run_ajax(data_obj);
+         });
+      });
+   }
+   exports.continue_click = continue_click;
+
+   return exports;
+}
+
+/*
+ * Load all the class/function here.
+ */
+var frontpage = Frontpage();
+
+/*
  * All event based listeners should be placed
  * in the document.ready(function()) or else
  * they may have a chance of incorrectly or
@@ -6,30 +58,9 @@
  */
 $(document).ready(function()
 {
+   // This the click event handler javascript.
    $('#continue-btn').click(function()
    {
-      $('#page-index').fadeOut(function()
-      {
-         $('#page-loading').fadeIn(function()
-         {
-            var data_obj = {};
-            data_obj.essay = $('#essay-text').val();
-
-            $.ajax({
-               url: 'index.php/api',
-               type: 'POST',
-               async: true,
-               dataType: 'json',
-               timeout: 25000,
-               data: {data: JSON.stringify(data_obj)},
-               success: function(data)
-               {
-                  alert(data);
-               }
-            });
-
-            // perform AJAX in here.
-         });
-      });
+      frontpage.continue_click();
    });
 });
